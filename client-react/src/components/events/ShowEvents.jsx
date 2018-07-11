247ca4876aee7603104f8f3791d33fa723e5fb17
dean <hindrough@gmail.com>
2018-07-11 11:29:41 -0400

    Update ShowEvents rendering.

--- client-react/src/components/events/ShowEvents.jsx
+++ client-react/src/components/events/ShowEvents.jsx
@@ -25,13 +25,13 @@ class ShowEvents extends React.Component {
     }
     render(){
       
-        console.log('rending events...');
+        console.log('updated 2002rending events...');
         return (
             <div id='events-show' className='column'>
                 <div id='events-entries'>
                 {this.state.events.map((event, index)=>{
                     return (
-                        <div key={event.id+index} className={event.featured ? 'event-item featured':'event-item'}>
+                        <div key={event.id+index} className={event.featured ? 'event-item yes featured':'event-item yes'}>
                            <Link to={`/event/${event.id}/rooms`}> <div className='logo'><img src={event.logo.url}/></div>
                             <div className='event-link'>{event.name}</div></Link>
                             <div className='event-description'>{event.description.substring(0,200)}...</div>
@@ -45,51 +45,3 @@ class ShowEvents extends React.Component {
     }
 }
 export default ShowEvents;
-
-/*
-{this.state.events.reduce((result, event, index)=>{
-                    if(event.logo != null){
-                        logo = event.logo.url;
-                    }
-                    type = index < 3 ? 'event-item featured':'event-item';
-                    result.push(
-                       
-                    );
-                    return result;
-                }, [])}
- <div key={event.id+index} className={type}>
-                           <Link to={`/event/${event.id}/rooms`}> <div className='logo'><img src={logo ? logo : '/assets/event-default.jpg'}/></div>
-                            <div className='event-link'>{event.name}</div></Link>
-                            <div className='event-description'>{event.description.substring(0,200)}...</div>
-                            <div className='event-start'>{event.start_date} to {event.end_date}, {event.start_time}-{event.end_time}</div>
-                        </div>
- if(index === 0){
-                        result.push(
-                            <div key={68+index} className='event-item'>
-                            <Link to={`/event/46995130701/rooms`}> <div className='logo'><img src='https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F46295521%2F260405887019%2F1%2Foriginal.jpg?w=480&auto=compress&rect=0%2C0%2C9000%2C4500&s=90fa22b347047e599be54dcfd8b5ddd0'/></div>
-                             <div className='event-link'>Coinvention</div></Link>
-                             <div className='event-description'>description...</div>
-                             <div className='event-start'>9/30</div>
-                             <div className='event-end'>to 9/30</div>
-                         </div>
-                        );
-                        result.push(
-                            <div key={69+index} className='event-item'>
-                            <Link to={`/event/4361188432/rooms`}> <div className='logo'><img src={logo ? logo : '/assets/event-default.jpg'}/></div>
-                             <div className='event-link'>DEFAULT</div></Link>
-                             <div className='event-description'>description...</div>
-                             <div className='event-start'>{event.start_date}</div>
-                             <div className='event-end'>to {event.end_date}</div>
-                         </div>
-                        );
-                        result.push(
-                            <div key={67+index} className='event-item'>
-                            <Link to={`/event/4361188432/rooms`}> <div className='logo'><img src={logo ? logo : '/assets/event-default.jpg'}/></div>
-                             <div className='event-link'>DEFAULT</div></Link>
-                             <div className='event-description'>description...</div>
-                             <div className='event-start'>{event.start_date}</div>
-                             <div className='event-end'>to {event.end_date}</div>
-                         </div>
-                        );
-                    }
-*/
